665f6ca05ad699364a77472503d02d0490b6aa4c
Anand Madhav Kulkarni <anandsprodigy@gmail.com>
2025-09-21 12:32:18 +0530

    excluded zip file from git ignore and added inline styling for home

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -162,7 +162,7 @@ export default function Home() {
       </main>
 
       {/* Footer */}
-      <footer className="bg-white border-t border-gray-200 mt-16">
+      <footer className="bg-white border-t border-gray-200 mt-16 bottom-0">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
           <div className="flex flex-col sm:flex-row justify-between items-center">
             <p className="text-sm text-gray-500">©{new Date().getFullYear()} Copyrights reserved.</p>
